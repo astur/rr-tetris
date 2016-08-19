4060eb1e73c81b8170fbb4c7d30d558965a3c140
astur <astur@yandex.ru>
2016-08-19 05:53:41 +0300

    reducer refactoring

--- src/reducer.js
+++ src/reducer.js
@@ -1,82 +1,56 @@
 import pieces from './pieces.js'
 
 function reducer(state = initialState, action) {
-    let newPosition, pieceCells, isOK, newActivePiece, newNextPiece, newNextCells
+    if ((state.position === null || state.gameOver || state.paused) && !~['STEP', 'RESTART', 'PAUSE'].indexOf(action.type)) {
+        return state
+    }
+    function isOk(pC){
+        return pC.every(v => state.cells[v[1]*10 + v[0]] !== 1) && pC.every(v => v[0] >= 0 && v[0] < 10 && v[1] < 25)
+    }
+    let newPosition, pieceCells, newActivePiece, newNextPiece, newNextCells
     switch (action.type) {
         case 'MOVE_LEFT':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            }
             newPosition = state.position.map((v, i) => i === 1 ? v : v - 1)
             pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+newPosition[0], v[1]+newPosition[1]])
-            isOK = (
-                pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                pieceCells.filter(v => v[0] < 0).length === 0
-            )
-            if (isOK) {
-                return Object.assign({}, state, {
-                    cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
-                    position: newPosition,
-                })
-            } else {
+            if (!isOk(pieceCells)) {
                 return state
             }
+            return Object.assign({}, state, {
+                cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
+                position: newPosition,
+            })
         case 'MOVE_RIGHT':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            }
             newPosition = state.position.map((v, i) => i === 1 ? v : v + 1)
             pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+newPosition[0], v[1]+newPosition[1]])
-            isOK = (
-                pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                pieceCells.filter(v => v[0] > 9).length === 0
-            )
-            if (isOK) {
-                return Object.assign({}, state, {
-                    cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
-                    position: newPosition,
-                })
-            } else {
+            if (!isOk(pieceCells)) {
                 return state
             }
+            return Object.assign({}, state, {
+                cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
+                position: newPosition,
+            })
         case 'MOVE_DOWN':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            }
             newPosition = state.position.map((v, i) => i === 0 ? v : v + 1)
             pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+newPosition[0], v[1]+newPosition[1]])
-            isOK = (
-                pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                pieceCells.filter(v => v[1] > 24).length === 0
-            )
-            if (isOK) {
-                return Object.assign({}, state, {
-                    cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
-                    position: newPosition,
-                })
-            } else {
+            if (!isOk(pieceCells)) {
                 return state
             }
+            return Object.assign({}, state, {
+                cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
+                position: newPosition,
+            })
         case 'ROTATE':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            }
             newActivePiece = pieces[state.activePiece].next
             pieceCells = pieces[newActivePiece].cells.map(v => [v[0]+state.position[0], v[1]+state.position[1]])
-            isOK = (
-                pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                pieceCells.filter(v => v[0] > 9 || v[0] < 0 || v[1] < 0 || v[1] > 24).length === 0
-            )
-            if (isOK) {
-                return Object.assign({}, state, {
-                    cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
-                    activePiece: newActivePiece,
-                })
-            } else {
+            if (!isOk(pieceCells)) {
                 return state
             }
+            return Object.assign({}, state, {
+                cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
+                activePiece: newActivePiece,
+            })
         case 'STEP':
-            if (state.gameOver === true || state.paused === true) {
+            if (state.gameOver || state.paused) {
                 return state
             }
             if (state.position === null) {
@@ -92,39 +66,27 @@ function reducer(state = initialState, action) {
                     nextPiece: newNextPiece,
                     gameOver: !(pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0),
                 })
-            } else {
-                newPosition = state.position.map((v, i) => i === 0 ? v : v + 1)
-                pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+newPosition[0], v[1]+newPosition[1]])
-                isOK = (
-                    pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                    pieceCells.filter(v => v[1] > 24).length === 0
-                )
-                if(isOK){
-                    return Object.assign({}, state, {
-                        cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
-                        position: newPosition,
-                    })
-                } else {
-                    pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+state.position[0], v[1]+state.position[1]])
-                    return Object.assign({}, state, {
-                        cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 1 : v === 2 ? 0 : v),
-                        position: null,
-                    })
-                }
             }
+            newPosition = state.position.map((v, i) => i === 0 ? v : v + 1)
+            pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+newPosition[0], v[1]+newPosition[1]])
+            if(isOk(pieceCells)){
+                return Object.assign({}, state, {
+                    cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 2 : v === 2 ? 0 : v),
+                    position: newPosition,
+                })
+            }
+            pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+state.position[0], v[1]+state.position[1]])
+            return Object.assign({}, state, {
+                cells: state.cells.map((v, i) => pieceCells.filter((v) => (v[1]*10 + v[0] === i)).length > 0 ? 1 : v === 2 ? 0 : v),
+                position: null,
+            })
         case 'DROP':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            }
-            isOK = true
+            let isOK = true
             let offset = 0
             while(isOK){
                 offset++
                 pieceCells = pieces[state.activePiece].cells.map(v => [v[0]+state.position[0], v[1]+state.position[1]+offset])
-                isOK = (
-                    pieceCells.filter(v => state.cells[v[1]*10 + v[0]]===1).length === 0 &&
-                    pieceCells.filter(v => v[1] > 24).length === 0
-                )
+                isOK = isOk(pieceCells)
             }
             offset--
             if (offset === 0) {
@@ -138,46 +100,33 @@ function reducer(state = initialState, action) {
                 position: null,
             })
         case 'RESTART':
-            if (state.gameOver === false) {
+            if (!state.gameOver) {
                 return state
-            } else {
-                return Object.assign({}, state, {
-                    cells: Array(250).fill(0),
-                    position: null,
-                    // activePiece: null,
-                    gameOver: false,
-                    paused: false,
-                    count: 0,
-                })
-            }
+            } 
+            return Object.assign({}, state, {
+                cells: Array(250).fill(0),
+                position: null,
+                gameOver: false,
+                count: 0,
+            })
         case 'PAUSE':
-            if (state.gameOver === true) {
+            if (state.gameOver) {
                 return state
-            } else {
-                return Object.assign({}, state, {
-                    paused: !state.paused,
-                })
             }
+            return Object.assign({}, state, {
+                paused: !state.paused,
+            })
         case 'CLEAR':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            } else {
-                let newCells = state.cells.filter(v => v !== 3)
-                let count = state.count + 25 - (newCells.length / 10)
-                return Object.assign({}, state, {
-                    cells: [].concat(Array(250 - newCells.length).fill(0), ...newCells),
-                    count: count,
-                })
-            }
+            let newCells = state.cells.filter(v => v !== 3)
+            return Object.assign({}, state, {
+                cells: [].concat(Array(250 - newCells.length).fill(0), ...newCells),
+                count: state.count + 25 - (newCells.length / 10),
+            })
         case 'HILIGHT':
-            if (state.position === null || state.gameOver === true || state.paused === true) {
-                return state
-            } else {
-                let cellRows = Array(25).fill(0).map((_,i) => state.cells.slice(i*10, i*10+10))
-                return Object.assign({}, state, {
-                    cells: [].concat(...(cellRows.map(v => v.every(v => v === 1) ? Array(10).fill(3) : v))),
-                })
-            }
+            let cellRows = Array(25).fill(0).map((_,i) => state.cells.slice(i*10, i*10+10))
+            return Object.assign({}, state, {
+                cells: [].concat(...(cellRows.map(v => v.every(v => v === 1) ? Array(10).fill(3) : v))),
+            })
         default:
             return state
     }
